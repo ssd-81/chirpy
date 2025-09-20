39a81d48ae1f54d2eb5aec66dfac6839b163bb71
ssd-81 <masterarc@yahoo.com>
2025-09-20 19:27:06 +0530

    added json encoding and decoding with error handling using proper payloads

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,13 +18,16 @@ func main() {
 	// serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
-	serveMux.HandleFunc("GET /app/healthz", func(w http.ResponseWriter, r *http.Request) {
+	serveMux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK")) // seems unlikely
 	})
-	serveMux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-	serveMux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
+	// introducing admin namespace
+	serveMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	serveMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	serveMux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+
 	server := http.Server{}
 	server.Handler = serveMux
 	server.Addr = ":8080"
@@ -32,11 +36,6 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 
 }
-
-// func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-// 	cfg.fileserverHits.Store(cfg.fileserverHits.Add(1))
-// 	return next
-// }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	// cfg.fileserverHits.Store(cfg.fileserverHits.Add(1))
@@ -48,10 +47,76 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
-	responseString := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	responseString := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
 	w.Write([]byte(responseString))
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type errorJson struct {
+		Error string `json:"error"`
+    }
+	type validJson struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		errPayload := errorJson {
+			Error: "something went wrong",
+		}
+		dat, err := json.Marshal(errPayload)
+		if err != nil {
+			w.WriteHeader(500)
+			log.Panicf("error while marshaling the json")
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+    	w.WriteHeader(400)
+    	w.Write(dat)
+		
+	}
+	if len(params.Body) > 140 {
+		errPayload := errorJson{
+			Error: "Chirp is too long",
+		}
+		dat, err := json.Marshal(errPayload)
+		if err != nil {
+			w.WriteHeader(500)
+			log.Panicf("error while marshaling the json")
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+    	w.WriteHeader(400)
+    	w.Write(dat)
+
+	} else {
+		validPayload := validJson{
+			Valid: true,
+		}
+		dat, err := json.Marshal(validPayload)
+		if err != nil {
+			w.WriteHeader(500)
+			log.Panicf("error while marshaling the json")
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+    	w.WriteHeader(200)
+    	w.Write(dat)
+
+	}
+
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
