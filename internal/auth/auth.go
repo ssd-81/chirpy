26852d93753710bd77d2f19a3761bcf44f0f8984
ssd-81 <masterarc@yahoo.com>
2025-10-24 13:02:44 +0530

    jwt validation and creation internal package created

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,24 +2,81 @@ package auth
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
+type MyCustomClaims struct {
+	jwt.RegisteredClaims
+}
+
 func HashPassword(password string) (string, error) {
-	hashedPassword, err:= bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", fmt.Errorf("error while hashing password")
 	}
 	strHash := string(hashedPassword)
-	return strHash, nil 
+	return strHash, nil
 }
-
 
 func CheckPasswordHash(password, hash string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	if err != nil {
 		return fmt.Errorf("the hashed password and entered password do not match")
 	}
-	return nil 
+	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	regClaims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, regClaims)
+	// signing the token
+	// made some mistakes here
+	// token.SignedString([]byte(tokenSecret))
+	// // jwt.NewWithClaims(jwt.SigningMethodHS256)
+	// return token.Raw, nil
+
+	signed, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signed, nil
+
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	// don't require a custom claim for now.
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(t *jwt.Token) (any, error) {
+		if t.Method != jwt.SigningMethodHS256 {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("error occured while parsing token")
+	} else if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok { // understand this line of code really well; type assertion
+		fmt.Println(claims.Issuer)
+	} else {
+		return uuid.Nil, fmt.Errorf("unknown claims type")
+	}
+
+	sub, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("error encountered while getting the subject")
+	}
+	userId, err := uuid.Parse(sub)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("invalid userId")
+	}
+	return userId, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,7 +2,9 @@ package auth
 
 import (
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -23,5 +25,33 @@ func TestUserPasswordAndHash(t *testing.T) {
 	if hashedPw == "" || err != nil {
 		t.Errorf(`HashPassword("something") = %q, %v, want match for %#q, nil`, hashedPw, err, userPassword)
 	}
+}
+
+func TestJWTCreation(t *testing.T) {
+	id := uuid.New()
+	secret := "amalgami"
+	duration := time.Second * 10
+
+	_, err := MakeJWT(id, secret, duration)
+	if err != nil {
+		t.Errorf(`Conversion failed: %v`, err)
+	}
 
 }
+
+func TestVerifyJWTToken(t * testing.T) {
+	id := uuid.New()
+	secret := "tensorflow"
+	duration := time.Second * 10
+
+	token, err := MakeJWT(id, secret, duration)
+	if err != nil {
+		t.Errorf(`Conversion failed: %v`, err)
+	}
+
+	verifyID, err := ValidateJWT(token, "tensorflow")
+	if verifyID != id || err != nil {
+		t.Errorf(`provided_token_id = %q, %v, want match for %#q, nil`, id, err, verifyID)
+	}
+}
+
